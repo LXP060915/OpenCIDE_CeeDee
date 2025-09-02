689057520c8758075258fbea9c13cda057eda336
李晓朋 <317669645@qq.com>
2025-09-02 16:27:41 +0800

    Add Release build files

---
+++ release/moc_mainwindow.cpp
@@ -0,0 +1,229 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.10)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../CIDE/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.12.10. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[32];
+    char stringdata0[384];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 7), // "newFile"
+QT_MOC_LITERAL(2, 19, 0), // ""
+QT_MOC_LITERAL(3, 20, 16), // "newFileInProject"
+QT_MOC_LITERAL(4, 37, 15), // "openFileRoutine"
+QT_MOC_LITERAL(5, 53, 8), // "filePath"
+QT_MOC_LITERAL(6, 62, 8), // "openFile"
+QT_MOC_LITERAL(7, 71, 8), // "saveFile"
+QT_MOC_LITERAL(8, 80, 10), // "saveFileAs"
+QT_MOC_LITERAL(9, 91, 7), // "exitApp"
+QT_MOC_LITERAL(10, 99, 22), // "chooseProjectDirectory"
+QT_MOC_LITERAL(11, 122, 11), // "defaultPath"
+QT_MOC_LITERAL(12, 134, 13), // "createProject"
+QT_MOC_LITERAL(13, 148, 7), // "setFont"
+QT_MOC_LITERAL(14, 156, 8), // "setColor"
+QT_MOC_LITERAL(15, 165, 8), // "closeTab"
+QT_MOC_LITERAL(16, 174, 5), // "index"
+QT_MOC_LITERAL(17, 180, 8), // "findText"
+QT_MOC_LITERAL(18, 189, 8), // "findNext"
+QT_MOC_LITERAL(19, 198, 12), // "findPrevious"
+QT_MOC_LITERAL(20, 211, 18), // "compileCurrentFile"
+QT_MOC_LITERAL(21, 230, 14), // "runCurrentFile"
+QT_MOC_LITERAL(22, 245, 18), // "collectSourceFiles"
+QT_MOC_LITERAL(23, 264, 7), // "dirPath"
+QT_MOC_LITERAL(24, 272, 18), // "showTabContextMenu"
+QT_MOC_LITERAL(25, 291, 3), // "pos"
+QT_MOC_LITERAL(26, 295, 13), // "renameTabFile"
+QT_MOC_LITERAL(27, 309, 13), // "aiImproveCode"
+QT_MOC_LITERAL(28, 323, 17), // "checkEnterPressed"
+QT_MOC_LITERAL(29, 341, 8), // "sendToAI"
+QT_MOC_LITERAL(30, 350, 8), // "userText"
+QT_MOC_LITERAL(31, 359, 24) // "clearConversationHistory"
+
+    },
+    "MainWindow\0newFile\0\0newFileInProject\0"
+    "openFileRoutine\0filePath\0openFile\0"
+    "saveFile\0saveFileAs\0exitApp\0"
+    "chooseProjectDirectory\0defaultPath\0"
+    "createProject\0setFont\0setColor\0closeTab\0"
+    "index\0findText\0findNext\0findPrevious\0"
+    "compileCurrentFile\0runCurrentFile\0"
+    "collectSourceFiles\0dirPath\0"
+    "showTabContextMenu\0pos\0renameTabFile\0"
+    "aiImproveCode\0checkEnterPressed\0"
+    "sendToAI\0userText\0clearConversationHistory"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      25,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  139,    2, 0x08 /* Private */,
+       3,    0,  140,    2, 0x08 /* Private */,
+       4,    1,  141,    2, 0x08 /* Private */,
+       6,    0,  144,    2, 0x08 /* Private */,
+       7,    0,  145,    2, 0x08 /* Private */,
+       8,    0,  146,    2, 0x08 /* Private */,
+       9,    0,  147,    2, 0x08 /* Private */,
+      10,    1,  148,    2, 0x08 /* Private */,
+      10,    0,  151,    2, 0x28 /* Private | MethodCloned */,
+      12,    0,  152,    2, 0x08 /* Private */,
+      13,    0,  153,    2, 0x08 /* Private */,
+      14,    0,  154,    2, 0x08 /* Private */,
+      15,    1,  155,    2, 0x08 /* Private */,
+      17,    0,  158,    2, 0x08 /* Private */,
+      18,    0,  159,    2, 0x08 /* Private */,
+      19,    0,  160,    2, 0x08 /* Private */,
+      20,    0,  161,    2, 0x08 /* Private */,
+      21,    0,  162,    2, 0x08 /* Private */,
+      22,    1,  163,    2, 0x08 /* Private */,
+      24,    1,  166,    2, 0x08 /* Private */,
+      26,    1,  169,    2, 0x08 /* Private */,
+      27,    0,  172,    2, 0x08 /* Private */,
+      28,    0,  173,    2, 0x08 /* Private */,
+      29,    1,  174,    2, 0x08 /* Private */,
+      31,    0,  177,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    5,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,   11,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   16,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::QStringList, QMetaType::QString,   23,
+    QMetaType::Void, QMetaType::QPoint,   25,
+    QMetaType::Void, QMetaType::Int,   16,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,   30,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->newFile(); break;
+        case 1: _t->newFileInProject(); break;
+        case 2: _t->openFileRoutine((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 3: _t->openFile(); break;
+        case 4: _t->saveFile(); break;
+        case 5: _t->saveFileAs(); break;
+        case 6: _t->exitApp(); break;
+        case 7: _t->chooseProjectDirectory((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 8: _t->chooseProjectDirectory(); break;
+        case 9: _t->createProject(); break;
+        case 10: _t->setFont(); break;
+        case 11: _t->setColor(); break;
+        case 12: _t->closeTab((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 13: _t->findText(); break;
+        case 14: _t->findNext(); break;
+        case 15: _t->findPrevious(); break;
+        case 16: _t->compileCurrentFile(); break;
+        case 17: _t->runCurrentFile(); break;
+        case 18: { QStringList _r = _t->collectSourceFiles((*reinterpret_cast< const QString(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< QStringList*>(_a[0]) = std::move(_r); }  break;
+        case 19: _t->showTabContextMenu((*reinterpret_cast< const QPoint(*)>(_a[1]))); break;
+        case 20: _t->renameTabFile((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 21: _t->aiImproveCode(); break;
+        case 22: _t->checkEnterPressed(); break;
+        case 23: _t->sendToAI((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 24: _t->clearConversationHistory(); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
+    &QMainWindow::staticMetaObject,
+    qt_meta_stringdata_MainWindow.data,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 25)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 25;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 25)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 25;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
